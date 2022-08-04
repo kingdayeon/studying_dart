cfd8034b8aeb7a9785dbc84770d2135b5461af68
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-08-04 18:44:30 +0900

    Create enum.dart

---
+++ enum.dart
@@ -0,0 +1,17 @@
+ enum Status{
+   approved,
+   pending,
+   rejected,
+ } 
+
+void main() {
+  Status status = Status.approved;
+  
+  if(status == Status.approved){
+    print('승인입니다.');
+  }else if(status == Status.pending){
+    print('대기입니다.');
+  }else{
+    print('거절입니다.');
+  }
+  }
