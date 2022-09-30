de1fbc894a39b658a0e2c21ae888d33e89a2221c
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-09-30 21:31:29 +0900

    Create toast.dart

---
+++ toast.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toast message',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Toast message'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: FlatButton(
+          onPressed: () {
+            flutterToast();
+          },
+          color: Colors.blue,
+          child: Text('Toast'),
+        )));
+  }
+}
+
+//토스트 함수 우리가 만들기
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: 'Flutter',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
+}
