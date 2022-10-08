8aa9d111d326e50713ae8d688abaa90d07a126b9
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-10-08 15:32:12 +0900

    Create screenSize.dart

---
+++ screenSize.dart
@@ -0,0 +1,4 @@
+MediaQuery.of(context).size.width;//폭 (LP단위)
+MediaQuery.of(context).size.height;//높이 (LP단위)
+MediaQuery.of(context).padding.top;//기기의 상단바 부분 높이 (LP단위)
+MediaQuery.of(context).devicePixelRatio;//이 기기는 1LP에 픽셀이 몇개 들어있는지
